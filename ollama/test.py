ede486b6861a878243c8cfb0ccaee03f565f2a86
iz4ack <luciaisaac.nv@gmail.com>
2025-01-29 16:49:18 +0100

    modificacion script para poder seleccionar modelo

--- ollama/test.py
+++ ollama/test.py
@@ -1,14 +1,28 @@
 import ollama   
 
+# Obtener la lista de modelos instalados
+def get_installed_models():
+    response: ollama.ListResponse = ollama.list()
+    return [model.model for model in response.models]
+
+
+# Mostrar modelos disponibles
+available_models = get_installed_models()
+
+print("Modelos disponibles:", ", ".join(available_models))
+modelo = input("Selecciona un modelo: ")
+
+# Verificar si el modelo seleccionado está instalado
+while modelo not in available_models:
+    print("Modelo no encontrado. Inténtalo de nuevo.")
+    modelo = input("Selecciona un modelo: ")
+
 message = input("> ")
 
-while message != 'exit':
-    response = ollama.chat(model='llama3.2:1b', messages=[
-    {
-        'role': 'user',
-        'content': message,
-    }], 
-    stream=True)
+while message.lower() != 'exit':
+    response = ollama.chat(model=modelo, messages=[
+        {'role': 'user', 'content': message}
+    ], stream=True)
 
     for chunk in response:
         print(chunk['message']['content'], end='', flush=True)
